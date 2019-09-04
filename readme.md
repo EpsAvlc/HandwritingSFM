3da553e1c69afe89843dd068390234ad0ed3776f
Ming Cao <cm9999@qq.com>
2019-09-04 15:09:23 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Handwriting SFM
+
+I prepare to write a simple sfm framework by myself.
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+int main(int argc, char** argv)
+{
+    
+}
